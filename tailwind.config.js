e2ec66cfd5aad09288251d05da6a5e029f1df60a
Sauhardya27 <sauhardyachakraborty2643@gmail.com>
2023-09-20 00:06:40 +0530

    Second commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,8 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        skin: '#EAB9A5',
-        purpleBlue: '#5D72E7',
+        skin: 'rgba(229, 188, 173, 0.71)',
+        purpleBlue: 'rgba(94, 89, 207, 0.71)',
         mix: '#a496c6',
         orskin: '#FCA087',
         bluePurple: '#6A7CE2',
@@ -21,12 +21,16 @@ module.exports = {
       },
 
       spacing: {
+        '14.5': '14.5rem',
+        '22': '22rem',
         '23': '23rem',
         '28.5': '28rem',
         '45': '45rem',
         '30': '30rem',
+        '34': '34rem',
         '50': '50rem',
-        '47': '47rem',
+        '48.5': '48rem',
+        '56.5': '56.5rem',
         '60.5': '60rem',
       },
 
